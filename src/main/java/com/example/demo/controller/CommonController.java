2f6e7137c45f127c9f2e96e556c16dd6bb6f7a2c
CrazyDiary <291845444@qq.com>
2019-05-01 15:40:42 +0800

    test

--- src/main/java/com/example/demo/controller/CommonController.java
+++ src/main/java/com/example/demo/controller/CommonController.java
@@ -9,29 +9,50 @@ import javax.servlet.http.HttpSession;
 public class CommonController {
     @Autowired
     public CommonService commonservice;
+
+//    默认页面
     @RequestMapping(value = "/", method = {RequestMethod.POST, RequestMethod.GET})
     public String login(){
-        return "/login";
+        return "/index";
     }
-    @RequestMapping(value = "/loginPage", method = {RequestMethod.POST, RequestMethod.GET})
+//   登录界面
+    @RequestMapping("/loginPage")
     public String loginPage(HttpServletRequest request, HttpSession session){
 
-        String tno = request.getParameter("tno");
+
+        String email = request.getParameter("email");
         String password = request.getParameter("password");
-        System.out.println("你输入的用户名为：" + tno);
+        System.out.println("你输入的用户名为：" + email);
         System.out.println("你输入的密码为：" + password);
-        String tname = commonservice.login(tno, password);
+        String tname = commonservice.login(email, password);
         System.out.println("del"+tname);
         session.setAttribute("tname", tname);
         if (tname == null) {
             return "/login";
         } else {
-            return "/test";
+            return "/index";
         }
     }
    @GetMapping("/loginIndex")
     public String loginIndex() {
         return "/test";
     }
+//    本地上传图片
+    @GetMapping("/location")
+    public String location(){
+        return "/location";
+    }
+
+//    相似图片
+    @GetMapping("/detail")
+    public String detail(){
+        return "/detail";
+    }
+
+//    照相机
+@GetMapping("/camera")
+public String camera(){
+    return "/camera";
+}
 
 }
---
+++ src/test/java/com/example/demo/a.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+
+
+public class a  {
+    public static void main(String[] args) {
+
+    }
+
+}
