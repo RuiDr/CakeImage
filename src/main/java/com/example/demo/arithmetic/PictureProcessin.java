86816e9cb01b50a4a10aa8365edd398a5d999e82
CrazyDiary <291845444@qq.com>
2019-05-05 22:46:07 +0800

    test

--- src/main/java/com/example/demo/arithmetic/PictureProcessin.java
+++ src/main/java/com/example/demo/arithmetic/PictureProcessin.java
@@ -1,4 +1,80 @@
 package com.example.demo.arithmetic;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
+import java.io.*;
 
 public class PictureProcessin {
+    public static final String path="C:/Users/CrazyCake/Pictures/testPic";
+
+    public static BufferedImage thumb(BufferedImage source,int width,int height,boolean b){
+//        targetW,targerH分别表示目标长和宽
+        int type=source.getType();
+        BufferedImage target=null;
+        double sx=(double)width/source.getWidth();
+        double sy=(double)height/source.getHeight();
+        if(b){
+            if(sx>sy){
+                sx=sy;
+                width=(int)(sx*source.getWidth());
+            }else{
+                sy=sx;
+                height=(int)(sy*source.getHeight());
+            }
+        }
+//     BufferedImage.TYPE_CUSTOM没有识别出图形类型，因此它必定是一个自定义图形
+        if(type==BufferedImage.TYPE_CUSTOM){
+//            colorModel存储图形颜色数据
+            ColorModel cm=source.getColorModel();
+            WritableRaster raster=cm.createCompatibleWritableRaster(width,height);
+            boolean alphaPremultiplied=cm.isAlphaPremultiplied();
+            target=new BufferedImage(cm,raster,alphaPremultiplied,null);
+        }else
+            target=new BufferedImage(width,height,type);
+            Graphics2D g=target.createGraphics();
+//            smoother than exlax;
+            g.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
+            g.drawRenderedImage(source, AffineTransform.getScaleInstance(sx,sy));
+            g.dispose();
+            return target;
+    }
+//    灰度值计算
+    public static int rgbToGray(int pixels){
+//        int _alpha=(pixels>>24)&0xFF;
+        int _red=(pixels>>16)&0xFF;
+        int _green=(pixels>>8)&0xFF;
+        int _blue=(pixels)&0xFF;
+        return (int)(0.3*_red+0.59*_green+0.11*_blue);
+    }
+    /**
+     * 读取JPEG图片
+     * @param filename 文件名
+     * @return BufferedImage 图片对象
+     */
+    public static BufferedImage readPNGImage(String filename)
+    {
+        try {
+            File inputFile = new File(filename);
+            BufferedImage sourceImage = ImageIO.read(inputFile);
+            return sourceImage;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+//    计算数组的平均值
+    public static int average(int[]piexls){
+        float m=0;
+        for (int i=0;i<piexls.length;++i){
+            m+=piexls[i];
+        }
+        m=m/piexls.length;
+        return (int)m;
+    }
+
 }
---
+++ src/main/java/com/example/demo/arithmetic/SimilarImageSearch.java
@@ -0,0 +1,147 @@
+package com.example.demo.arithmetic;
+
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimilarImageSearch {
+    public static void main(String[] args) {
+        List<String>hashCodes=new ArrayList<>();
+        String fileName= PictureProcessin.path+"/images/";
+        String hashCode=null;
+        for (int i = 0; i < 10; i++)
+        {
+            hashCode = produceFingerPrint(fileName  + (i + 1) + ".jpg");
+            hashCodes.add(hashCode);
+        }
+        System.out.println("Resources: ");
+        System.out.println(hashCodes);
+        System.out.println();
+
+        String sourceHashCode = produceFingerPrint(fileName + "source.jpg");
+        System.out.println("Source: ");
+        System.out.println(sourceHashCode);
+        System.out.println();
+        for (int i = 0; i < hashCodes.size(); i++)
+        {
+            int difference = hammingDistance(sourceHashCode, hashCodes.get(i));
+            if(difference==0){
+                System.out.println("source.jpg图片跟example"+(i+1)+".jpg一样");
+            }else if(difference<=5){
+                System.out.println("source.jpg图片跟example"+(i+1)+".jpg非常相似");
+            }else if(difference<=10){
+                System.out.println("source.jpg图片跟example"+(i+1)+".jpg有点相似");
+            }else if(difference>10){
+                System.out.println("source.jpg图片跟example"+(i+1)+".jpg完全不一样");
+            }
+            System.out.println(difference);
+        }
+    }
+//    计算汉明距离
+    public static int hammingDistance(String sourceHashCode,String hashCode){
+        int difference=0;
+        int len=sourceHashCode.length();
+        for (int i=0;i<len;i++){
+            if(sourceHashCode.charAt(i)!=hashCode.charAt(i)){
+                difference++;
+            }
+        }
+        return difference;
+    }
+//    生成图片指纹
+    public static String produceFingerPrint(String filename){
+        System.out.println("filename is "+filename);
+        BufferedImage source=PictureProcessin.readPNGImage(filename);//读文件
+        int width=8;
+        int height=8;
+//        第一步，缩小尺寸
+        BufferedImage thumb=PictureProcessin.thumb(source,width,height,false);
+//        第二步，简化色彩
+//        将缩小后的图片，转为64级灰度，也就是说，所有像素点总共只有64种颜色.
+        int []pixels=new int[width*height];
+        for (int i=0;i<width;i++){
+            for (int j=0;j<height;j++){
+                pixels[i*height+j]=PictureProcessin.rgbToGray(thumb.getRGB(i,j));
+            }
+        }
+//        第三步，计算平均值
+        int avgPixel =PictureProcessin.average(pixels);
+//        第四步，比较像素的灰度
+//        将每个像素的灰度，与平均值进行比较，大于或等于平均值，记为1：小于平均值，记为0.
+      int[]comps=new int[width*height];
+      for (int i=0;i<comps.length;i++){
+          if(pixels[i]>=avgPixel){
+              comps[i]=1;
+          }else{
+              comps[i]=0;
+          }
+      }
+//      第五步，计算哈希值
+//       将上一步的比较结果，组合在一起，就构成了一个64位的整数，这就是这张图片的指纹
+     StringBuffer hashCode=new StringBuffer();
+      for (int i=0;i<comps.length;i+=4){
+          int result=comps[i]*(int)Math.pow(2,3)+comps[i+1]*(int)Math.pow(2,2)+comps[i + 2] * (int) Math.pow(2, 1) + comps[i + 3];
+          hashCode.append(binaryToHex(result));
+      }
+//       得到指纹以后，就可以对比不同的图片，看看64位中有多少位是不一样的。
+      return hashCode.toString();
+    }
+//    二进制转为十六进制
+    private static char binaryToHex(int binary){
+        char ch=' ';
+        switch (binary){
+            case 0:
+                ch = '0';
+                break;
+            case 1:
+                ch = '1';
+                break;
+            case 2:
+                ch = '2';
+                break;
+            case 3:
+                ch = '3';
+                break;
+            case 4:
+                ch = '4';
+                break;
+            case 5:
+                ch = '5';
+                break;
+            case 6:
+                ch = '6';
+                break;
+            case 7:
+                ch = '7';
+                break;
+            case 8:
+                ch = '8';
+                break;
+            case 9:
+                ch = '9';
+                break;
+            case 10:
+                ch = 'a';
+                break;
+            case 11:
+                ch = 'b';
+                break;
+            case 12:
+                ch = 'c';
+                break;
+            case 13:
+                ch = 'd';
+                break;
+            case 14:
+                ch = 'e';
+                break;
+            case 15:
+                ch = 'f';
+                break;
+            default:
+                ch = ' ';
+        }
+        return ch;
+    }
+}
