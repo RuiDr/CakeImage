7b6b86e3fcd567b12496f6a92b59f3a398b23044
CrazyDiary <291845444@qq.com>
2019-05-10 09:31:31 +0800

    test

--- src/main/java/com/example/cakeImage/controller/ArithmeticController.java
+++ src/main/java/com/example/cakeImage/controller/ArithmeticController.java
@@ -1,19 +1,16 @@
 package com.example.cakeImage.controller;
-
-
-import ch.qos.logback.core.util.FileUtil;
 import com.example.cakeImage.arithmetic.SimilarImageSearch;
+import com.example.cakeImage.tools.Utility;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.apache.commons.io.*;
-import java.lang.String.*;
+import org.springframework.web.servlet.ModelAndView;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.io.File;
-import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,50 +19,40 @@ public class ArithmeticController {
 
     @ResponseBody
     @RequestMapping("/perception")
-    public void perception( @RequestParam( value = "file")MultipartFile file,String filePath,HttpServletRequest request, HttpSession session){
+    public String perception(@RequestParam( value = "file")MultipartFile file, String filePath, HttpServletRequest request, HttpSession session){
 
-      String path="G:\\java\\webprojects\\CakeImage\\sourceImages\\";
-      System.out.println("filePath is "+filePath);
-      File filePa=new File(path);
-      if(!filePa.exists()){
-          filePa.mkdir();
-      }
-      String fileNameA="";
-      if(file.isEmpty()){
-          return;
-      }
-      String suf=file.getOriginalFilename().substring(file.getOriginalFilename().indexOf(".")+1);
-      fileNameA="source"+"."+suf;
-      try{
-          FileUtils.writeByteArrayToFile(new File(path + fileNameA),
-                  file.getBytes());
+//        获取目标图片的地址
+      String sourceImagePath=  Utility.tool(file,filePath);
+      System.out.println("sourceImagePath is "+sourceImagePath);
 
-          System.out.println("path is "+path+fileNameA);
-//          org.aspectj.util.FileUtil.canWriteFile(new File(path+fileNameA));
+//        样本集的所有图片的指纹
 
-      } catch (Exception e) {
-          e.printStackTrace();
-      }
+        List<String> list=new ArrayList<>();
+        list=SimilarImageSearch.produceAllImages(10);
+
+//获取目标图片的指纹
+
+        String sourceCode = SimilarImageSearch.produceFingerPrint(sourceImagePath);
+        System.out.println("sourceCode is "+sourceCode);
 
 
-//        List<String> list=new ArrayList<>();
-//
-////        获取所有图片的指纹
-//        list=SimilarImageSearch.produceAllImages(10);
-//        System.out.println(list.toString());
-//
-////获取目标图片的指纹
-//        try {
-////            String basepath=filePath.replaceAll("\\\\", "/");
-////            basepath=URLDecoder.decode(basepath,"utf-8");
-//            String sourceCode = SimilarImageSearch.produceFingerPrint(filePath);
-//            System.out.println(sourceCode);
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//
-////        计算所有图片与目标图片的汉明距离
-//
-//        return "/detail";
+//     计算所有图片与目标图片的汉明距离
+        ArrayList<String >ImagePath=new ArrayList<>();
+       ImagePath= Utility.hammingDistance(sourceCode,list);
+        System.out.println(ImagePath.toString());
+        ModelAndView modelAndView=new ModelAndView();
+//          String base64=  Utility.urlToBase64(sourceImagePath);
+        System.out.print("base64 is "+file.getOriginalFilename());
+      String fileName=file.getOriginalFilename();
+      if (fileName!=null)
+        session.setAttribute("fileName",fileName);
+        if(ImagePath.size()!=0){
+            modelAndView.addObject("ImagePath",ImagePath);
+            session.setAttribute("ImagePath",ImagePath);
+
+        }else{
+        }
+
+        return "/detail";
     }
 }
---
+++ src/main/java/com/example/cakeImage/tools/Utility.java
@@ -0,0 +1,69 @@
+package com.example.cakeImage.tools;
+
+import com.example.cakeImage.arithmetic.SimilarImageSearch;
+import org.apache.commons.io.FileUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import com.mchange.util.Base64Encoder;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
+public class Utility  {
+    static String  path="G:\\java\\webprojects\\CakeImage\\src\\main\\resources\\static\\images\\";
+    public static String tool(MultipartFile file, String filePath) {
+
+        System.out.println("filePath is "+filePath);
+        File filePa=new File(path);
+        if(!filePa.exists()){
+            filePa.mkdir();
+        }
+        String fileNameA="";
+        if(file.isEmpty()){
+            return null;
+        }
+        String suf=file.getOriginalFilename().substring(file.getOriginalFilename().indexOf(".")+1);
+        fileNameA="source"+"."+suf;
+        try{
+            FileUtils.writeByteArrayToFile(new File(path + fileNameA),
+                    file.getBytes());
+
+            System.out.println("path is "+path+fileNameA);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return path+fileNameA;
+    }
+
+    public static ArrayList<String> hammingDistance(String sourceCode, List<String> list) {
+        ArrayList<String >list1=new ArrayList<>();
+        int distance=0;
+        for (int i=0;i<list.size();i++){
+            distance=SimilarImageSearch.hammingDistance(sourceCode,list.get(i));
+//            图片有点相似
+            if (distance<10){
+                list1.add(path+i+".jpg");
+            }
+        }
+//        返回的图片编号的字符串形式
+        return list1;
+    }
+    public static String urlToBase64(String path){
+        String base64Img="";
+        try {
+            File file = new File(path);
+            FileInputStream is = new FileInputStream(file);
+            byte[] data = new byte[is.available()];
+            is.read(data);
+            is.close();
+             base64Img = Base64.getEncoder().encodeToString(data);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return base64Img;
+    }
+}
